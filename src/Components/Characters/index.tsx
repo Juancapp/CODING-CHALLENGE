e3168c5b700408950095a98a33ecaf9d043d6c75
Juancapp <juan.cappuccio@radiumrocket.com>
2023-08-31 16:25:58 -0300

    feature/RM: make pagination

--- src/Components/Characters/index.tsx
+++ src/Components/Characters/index.tsx
@@ -1,22 +1,54 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./characters.module.css";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Card from "../Shared/Card";
 
 const Characters = () => {
   const navigate = useNavigate();
+  const [characters, setCharacters] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await axios.get(
+          `${import.meta.env.VITE_API_URL}/characters/name/${page}`
+        );
+        setCharacters(response?.data?.data);
+      } catch (error) {
+        alert(error);
+      }
+    }
+    fetchData();
+  }, [page]);
+
+  const handleChangePage = (number: 1 | -1) => {
+    if (number === -1 && page > 1) setPage(page + number);
+    if (number === 1 && page < 43) setPage(page + number);
+  };
 
   const handleNavigate = (path: string) => {
     navigate(path);
   };
 
   return (
-    <div>
+    <div className={styles.charactersContainer}>
       <p
         className={styles.favChar}
         onClick={() => handleNavigate("../favchar")}
       >
         Favorite Characters
       </p>
-      <span>Characters</span>
+      <div className={styles.characters}>
+        {characters?.map((character) => {
+          return <Card data={character} />;
+        })}
+      </div>
+      <div className={styles.buttonsContainer}>
+        <button onClick={() => handleChangePage(-1)}>Página anterior</button>
+        <button onClick={() => handleChangePage(1)}>Página siguiente</button>
+      </div>
     </div>
   );
 };
--- src/Components/Shared/Card/index.tsx
+++ src/Components/Shared/Card/index.tsx
@@ -1,34 +1,38 @@
 import styles from "./card.module.css";
 
 interface CardProps {
-  id?: number;
-  name: string;
-  status?: string;
-  species?: string;
-  type?: string;
-  gender: string;
-  origin?: {
+  data: {
+    id?: number;
     name: string;
-    url: string;
-  };
-  location: {
-    name: string;
+    status?: string;
+    species?: string;
+    type?: string;
+    gender: string;
+    origin?: {
+      name: string;
+      url: string;
+    };
+    location: {
+      name: string;
+      url?: string;
+    };
+    image: string;
+    episode?: string[];
     url?: string;
+    created?: string;
   };
-  image: string;
-  episode?: string[];
-  url?: string;
-  created?: string;
 }
 
 const Card = (props: CardProps) => {
-  const { name, gender, location, image } = props;
+  const { data } = props;
+  const { name, gender, location, image } = data;
 
   const locationName = location.name;
 
   return (
     <div className={styles.cardContainer}>
       <img src={image} alt={name} />
+      <span>Name: {name}</span>
       <span>Gender: {gender}</span>
       <span>Planet: {locationName}</span>
     </div>
